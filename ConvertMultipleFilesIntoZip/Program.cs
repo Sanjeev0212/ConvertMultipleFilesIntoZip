22d555bb32a4f8bdeeea0d666cca05aed14ebd7c
Sanjeev0212 <ssinghkushvaha@gmail.com>
2022-12-26 13:03:50 +0530

    Add project files.

---
+++ ConvertMultipleFilesIntoZip/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace ConvertMultipleFilesIntoZip
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            string zipPath = @"d:\result" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".zip";
+
+            using (ZipArchive archive = ZipFile.Open(zipPath, ZipArchiveMode.Create))
+            {
+
+                archive.CreateEntryFromFile(@"d:\file3.txt", "file3.txt");
+                archive.CreateEntryFromFile(@"d:\file1.txt", "file1.txt");
+            }
+            Console.WriteLine(zipPath + " is created.");
+
+        }
+
+        
+    }
+}
+
