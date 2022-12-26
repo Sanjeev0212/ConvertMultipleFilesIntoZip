004c589ef6e6c490b11a0d493cf61ef71e1b7a7f
Sanjeev0212 <ssinghkushvaha@gmail.com>
2022-12-27 00:23:44 +0530

    Exception handling is working

--- ConvertMultipleFilesIntoZip/Program.cs
+++ ConvertMultipleFilesIntoZip/Program.cs
@@ -5,22 +5,48 @@ using System.IO.Compression;
 namespace ConvertMultipleFilesIntoZip
 {
     internal class Program
-    {   
+    {
         static void Main(string[] args)
         {
-            string zipPath = @"d:\result" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".zip";
-
-            using (ZipArchive archive = ZipFile.Open(zipPath, ZipArchiveMode.Create))
+            try
             {
+                Console.WriteLine("..........Convert multiple files into a zip file...........");
+                Console.WriteLine();
+                Console.WriteLine();
+                string zipPath = @"d:\result" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + ".zip";
 
-                archive.CreateEntryFromFile(@"d:\file3.txt", "file3.txt");
-                archive.CreateEntryFromFile(@"d:\file1.txt", "file1.txt");
+                using (ZipArchive archive = ZipFile.Open(zipPath, ZipArchiveMode.Create))
+                {
+                    Console.WriteLine("Enter the 1st file path, Which you want to add: ");
+                    string sourcePath1 = Console.ReadLine();
+                    Console.WriteLine("Enter the 2st file path, Which you want to add: ");
+                    string sourcePath2 = Console.ReadLine();
+                    Console.WriteLine("Enter the 3st file path, Which you want to add: ");
+                    string sourcePath3 = Console.ReadLine();
+                    Console.WriteLine("Enter the 4st file path, Which you want to add: ");
+                    string sourcePath4 = Console.ReadLine();
+                    archive.CreateEntryFromFile(sourcePath1, "file1.txt");
+                    archive.CreateEntryFromFile(sourcePath2, "file2.txt");
+                    archive.CreateEntryFromFile(sourcePath3, "file1.txt");
+                    archive.CreateEntryFromFile(sourcePath4, "sanju1.nupkg");
+
+                    /*archive.CreateEntryFromFile(@"d:\file1.txt", "file1.txt");
+                    archive.CreateEntryFromFile(@"c:\Intel\file2.txt", "file2.txt");
+                    archive.CreateEntryFromFile(@"d:\file1.txt", "file1.txt");
+                    archive.CreateEntryFromFile(@"d:\sanju1.nupkg", "sanju1.nupkg");*/
+
+                }
+                Console.WriteLine("Zip file path is " + zipPath + " .");
             }
-            Console.WriteLine(zipPath + " is created.");
+            catch (FileNotFoundException dirEx)
+            {
+                Console.WriteLine("File does not exist: " + dirEx.Message);
+            }
+
+
 
         }
 
-        
     }
-}
+} 
 
